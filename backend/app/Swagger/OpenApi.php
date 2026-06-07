1b5b82bf5d1ba7642989ba33da1fa66f616d6503
adolfodev10 <adolfomanueldev10@gmail.com>
2026-06-07 17:44:07 +0100

    feat(OpenApi): add OpenApi documentation for Product CRUD API

---
+++ backend/app/Swagger/OpenApi.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @OA\Info(
+ *     title="Product CRUD API",
+ *     version="1.0.0",
+ *     description="API para sistema de gerenciamento de produtos"
+ * )
+ * 
+ * @OA\Server(
+ *     url="http://localhost:8000/api",
+ *     description="Servidor Local"
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ */
